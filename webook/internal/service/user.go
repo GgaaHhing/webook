7ad53fa9c854a19e7fcc1db699c902ab7f3135a0
思远久安 <2900412443@qq.com>
2024-12-16 12:08:57 +0800

    commit

---
+++ webook/internal/service/user.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"basic-go/webook/internal/domain"
+	"basic-go/webook/internal/repository"
+	"context"
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrDuplicateEmail        = repository.ErrDuplicateEmail
+	ErrInvalidUserOrPassword = errors.New("用户不存在或者密码不对")
+)
+
+type UserService struct {
+	repo *repository.UserRepository
+}
+
+func NewUserService(repo *repository.UserRepository) *UserService {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+func (svc *UserService) Signup(ctx context.Context, u domain.User) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hash)
+	return svc.repo.Create(ctx, u)
+}
